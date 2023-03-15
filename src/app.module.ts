ed556ce205832152c2724588c2e6bd0c9b342513
Aibek <aibek.abdykasymov@gmail.com>
2023-03-15 15:28:33 +0600

    Deleted unused app controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmAsyncConfig } from './config/typeorm.config';
 import { TaskModule } from './task/task.module';
@@ -10,7 +8,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forRootAsync(typeOrmAsyncConfig), AuthModule, TaskModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
