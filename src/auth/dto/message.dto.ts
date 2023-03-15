95f68bc26f9adab48b00d4e7b882df784a590440
Aibek <aibek.abdykasymov@gmail.com>
2023-03-15 15:28:43 +0600

    Added message dto

---
+++ src/auth/dto/message.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class MessageDto {
+  @ApiProperty()
+  message: string;
+  constructor(message: string) {
+    this.message = message;
+  }
+}
